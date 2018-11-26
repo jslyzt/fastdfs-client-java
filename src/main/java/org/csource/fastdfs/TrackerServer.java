b45e28b030ce600955af7efe3c180c40a814fb45
zhoutao <zhout@hiscene.com>
2018-11-26 15:59:41 +0800

    格式化更新

--- src/main/java/org/csource/fastdfs/TrackerServer.java
+++ src/main/java/org/csource/fastdfs/TrackerServer.java
@@ -21,61 +21,60 @@ import java.net.Socket;
  * @version Version 1.11
  */
 public class TrackerServer {
-  protected Socket sock;
-  protected InetSocketAddress inetSockAddr;
+    protected Socket sock;
+    protected InetSocketAddress inetSockAddr;
 
-  /**
-   * Constructor
-   *
-   * @param sock         Socket of server
-   * @param inetSockAddr the server info
-   */
-  public TrackerServer(Socket sock, InetSocketAddress inetSockAddr) {
-    this.sock = sock;
-    this.inetSockAddr = inetSockAddr;
-  }
-
-  /**
-   * get the connected socket
-   *
-   * @return the socket
-   */
-  public Socket getSocket() throws IOException {
-    if (this.sock == null) {
-      this.sock = ClientGlobal.getSocket(this.inetSockAddr);
+    /**
+     * Constructor
+     *
+     * @param sock         Socket of server
+     * @param inetSockAddr the server info
+     */
+    public TrackerServer(Socket sock, InetSocketAddress inetSockAddr) {
+        this.sock = sock;
+        this.inetSockAddr = inetSockAddr;
     }
 
-    return this.sock;
-  }
-
-  /**
-   * get the server info
-   *
-   * @return the server info
-   */
-  public InetSocketAddress getInetSocketAddress() {
-    return this.inetSockAddr;
-  }
-
-  public OutputStream getOutputStream() throws IOException {
-    return this.sock.getOutputStream();
-  }
-
-  public InputStream getInputStream() throws IOException {
-    return this.sock.getInputStream();
-  }
-
-  public void close() throws IOException {
-    if (this.sock != null) {
-      try {
-        ProtoCommon.closeSocket(this.sock);
-      } finally {
-        this.sock = null;
-      }
+    /**
+     * get the connected socket
+     *
+     * @return the socket
+     */
+    public Socket getSocket() throws IOException {
+        if (this.sock == null) {
+            this.sock = ClientGlobal.getSocket(this.inetSockAddr);
+        }
+        return this.sock;
     }
-  }
 
-  protected void finalize() throws Throwable {
-    this.close();
-  }
+    /**
+     * get the server info
+     *
+     * @return the server info
+     */
+    public InetSocketAddress getInetSocketAddress() {
+        return this.inetSockAddr;
+    }
+
+    public OutputStream getOutputStream() throws IOException {
+        return this.sock.getOutputStream();
+    }
+
+    public InputStream getInputStream() throws IOException {
+        return this.sock.getInputStream();
+    }
+
+    public void close() throws IOException {
+        if (this.sock != null) {
+            try {
+                ProtoCommon.closeSocket(this.sock);
+            } finally {
+                this.sock = null;
+            }
+        }
+    }
+
+    protected void finalize() throws Throwable {
+        this.close();
+    }
 }
